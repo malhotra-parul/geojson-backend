8b5f7c5f998b1b43c97d88ad18c009cc37fe9c20
malhotra-parul <parulm.business@gmail.com>
2020-10-06 14:10:48 +0530

    created db

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-const config = require("config");
-const mongoUri = config.get("mongoUri");
+require('dotenv').config();
+// const config = require("config");
+const mongoUri = process.env.mongoUri;
 mongoose.connect(mongoUri, {useNewUrlParser: true , useUnifiedTopology: true }, (err) =>{
     if(err) throw err;
     console.log("DB connected!");
